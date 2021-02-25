a870672457a3963b3818359e42918b255cd1606e
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-02-25 11:00:11 -0300

    feat: add deposit function to Account

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,4 +1,7 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: CreateUser
   defdelegate create_user(params), to: CreateUser, as: :call
+
+  alias Rocketpay.Accounts.Deposit
+  defdelegate deposit(params), to: Deposit, as: :call
 end
