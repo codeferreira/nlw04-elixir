63c11f3ad06a4796d646e84504b9e04b6688e460
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-02-26 15:37:25 -0300

    feat: add delegate to Withdraw and Transaction

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -2,6 +2,8 @@ defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: CreateUser
   defdelegate create_user(params), to: CreateUser, as: :call
 
-  alias Rocketpay.Accounts.Deposit
+  alias Rocketpay.Accounts.{Deposit, Withdraw, Transaction}
   defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
 end
