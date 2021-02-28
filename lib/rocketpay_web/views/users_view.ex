3ae5361f7059d6fc5c6ac259a00e1c18a5df2d48
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-02-28 16:53:12 -0300

    feat: add users views based on new controllers

--- lib/rocketpay_web/views/users_view.ex
+++ lib/rocketpay_web/views/users_view.ex
@@ -1,12 +1,33 @@
 defmodule RocketpayWeb.UsersView do
+  use RocketpayWeb, :view
   alias Rocketpay.{Account, User}
+  alias RocketpayWeb.UsersView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UsersView, "user.json", as: :user)}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UsersView, "user.json", as: :user)}
+  end
+
+  def render("user.json", %{
+        user: %User{
+          id: id,
+          name: name,
+          nickname: nickname
+        }
+      }) do
+    %{id: id, name: name, nickname: nickname}
+  end
 
   def render("create.json", %{
-      user: %User{
-        id: id,
-        name: name,
-        nickname: nickname,
-        account: %Account{id: account_id, balance: account_balance}}
+        user: %User{
+          id: id,
+          name: name,
+          nickname: nickname,
+          account: %Account{id: account_id, balance: account_balance}
+        }
       }) do
     %{
       message: "User created",
@@ -16,9 +37,8 @@ defmodule RocketpayWeb.UsersView do
         nickname: nickname,
         account: %{
           id: account_id,
-          balance: account_balance,
+          balance: account_balance
         }
-
       }
     }
   end
