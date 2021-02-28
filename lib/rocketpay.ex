c71a294173224df6e7389aadc9517b657f5eb963
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-02-28 16:35:16 -0300

    feat: add users delegate functions

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,6 +1,12 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: CreateUser
   defdelegate create_user(params), to: CreateUser, as: :call
+  alias Rocketpay.Users.Index, as: GetUsers
+  defdelegate get_users(), to: GetUsers, as: :call
+  alias Rocketpay.Users.Show, as: GetUser
+  defdelegate get_user(params), to: GetUser, as: :call
+  alias Rocketpay.Users.Delete, as: DeleteUser
+  defdelegate delete_user(params), to: DeleteUser, as: :call
 
   alias Rocketpay.Accounts.{Deposit, Withdraw, Transaction}
   defdelegate deposit(params), to: Deposit, as: :call
