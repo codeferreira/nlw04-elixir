529ad4614cf27f42e10e334593406c183c63b429
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-02-28 16:37:09 -0300

    feat: add password confimation and account privacy

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -4,38 +4,59 @@ defmodule Rocketpay.User do
   import Ecto.Changeset
 
   alias Ecto.Changeset
-  alias Rocketpay.Account
+  alias Rocketpay.{Account, Encryption, Friendship}
 
   @primary_key {:id, :binary_id, autogenerate: true}
-  @required_params [:name, :age, :email, :password, :nickname]
+  @required_params [
+    :name,
+    :age,
+    :email,
+    :password,
+    :password_confirmation,
+    :nickname,
+    :is_private
+  ]
 
   schema "users" do
     field :name, :string
     field :age, :integer
     field :email, :string
     field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
+    field :is_private, :boolean, default: false
     has_one :account, Account
+    has_many :friends, Friendship.Friend
 
     timestamps()
   end
 
-  def changeset(params) do
-    %__MODULE__{}
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
     |> cast(params, @required_params)
     |> validate_required(@required_params)
     |> validate_length(:password, min: 6)
+    |> validate_confirmation(:password)
     |> validate_number(:age, greater_than_or_equal_to: 18)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
     |> unique_constraint([:nickname])
-    |> put_password_hash()
+    |> downcase_username()
+    |> encrypt_password()
   end
 
-  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
-    change(changeset, Bcrypt.add_hash(password))
+  defp encrypt_password(changeset) do
+    case changeset do
+      %Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Encryption.hash_password(password))
+
+      _ ->
+        changeset
+    end
   end
 
-  defp put_password_hash(changeset), do: changeset
+  defp downcase_username(changeset) do
+    update_change(changeset, :nickname, &String.downcase/1)
+  end
 end
