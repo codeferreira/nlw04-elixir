01ab611c529b82675e468eef9e65edf87156ae4b
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-02-28 16:39:21 -0300

    refactor: remove value on transaction

--- lib/rocketpay/accounts/transaction/response.ex
+++ lib/rocketpay/accounts/transaction/response.ex
@@ -1,13 +1,12 @@
 defmodule Rocketpay.Accounts.Transactions.Response do
   alias Rocketpay.Account
 
-  defstruct [:from, :to, :value]
+  defstruct [:from, :to]
 
-  def build(%Account{} = from_account, %Account{} = to_account, value) do
+  def build(%Account{} = from_account, %Account{} = to_account) do
     %__MODULE__{
       from: from_account,
-      to: to_account,
-      value: value
+      to: to_account
     }
   end
 end
