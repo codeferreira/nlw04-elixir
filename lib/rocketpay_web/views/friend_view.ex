5b8d07a3425ed5422d736026192bf2054afc7f56
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-03-01 21:00:46 -0300

    feat: add friend view data correctly

--- lib/rocketpay_web/views/friend_view.ex
+++ lib/rocketpay_web/views/friend_view.ex
@@ -1,6 +1,8 @@
 defmodule RocketpayWeb.FriendView do
   use RocketpayWeb, :view
   alias RocketpayWeb.FriendView
+  alias Rocketpay.User
+  alias Rocketpay.Friendship.Friend
 
   def render("index.json", %{friends: friends}) do
     %{data: render_many(friends, FriendView, "friend.json")}
@@ -10,7 +12,25 @@ defmodule RocketpayWeb.FriendView do
     %{data: render_one(friend, FriendView, "friend.json")}
   end
 
-  def render("friend.json", %{friend: friend}) do
-    %{id: friend.id}
+  def render("friend.json", %{
+        friend: %Friend{
+          id: id,
+          user: %User{id: user_id, name: user_name, nickname: user_nickname},
+          addresse: %User{id: addresse_id, name: addresse_name, nickname: addresse_nickname}
+        }
+      }) do
+    %{
+      id: id,
+      user: %{
+        id: user_id,
+        name: user_name,
+        nickname: user_nickname
+      },
+      addresse: %{
+        id: addresse_id,
+        name: addresse_name,
+        nickname: addresse_nickname
+      }
+    }
   end
 end
