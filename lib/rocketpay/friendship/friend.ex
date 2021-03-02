d675882467df16fa490538ce050000531427f67a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-03-01 21:00:16 -0300

    feat: add schema and constraint to validate unique friendship

--- lib/rocketpay/friendship/friend.ex
+++ lib/rocketpay/friendship/friend.ex
@@ -4,21 +4,21 @@ defmodule Rocketpay.Friendship.Friend do
   alias Rocketpay.User
 
   @primary_key {:id, :binary_id, autogenerate: true}
-
   @required_params [:user_id, :addresse_id, :accepted]
+  @foreign_key_type :binary_id
 
   schema "friends" do
-    field :addresse_id, :binary_id
-    belongs_to :user, User, foreign_key: :user_id, type: :binary_id
     field :accepted, :boolean, default: false
+    belongs_to :user, User, foreign_key: :user_id
+    belongs_to :addresse, User, foreign_key: :addresse_id
 
     timestamps()
   end
 
-  @doc false
   def changeset(struct \\ %__MODULE__{}, params) do
     struct
     |> cast(params, @required_params)
     |> validate_required(@required_params)
+    |> unique_constraint([:user_id, :addresse_id], name: :unique_friendship)
   end
 end
