bff2477c11f7f6391717249beafb2fa561fd06bb
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-03-02 20:31:16 -0300

    feat: format transaction return

--- lib/rocketpay/accounts/transaction/response.ex
+++ lib/rocketpay/accounts/transaction/response.ex
@@ -1,12 +1,14 @@
 defmodule Rocketpay.Accounts.Transactions.Response do
   alias Rocketpay.Account
 
-  defstruct [:from, :to]
+  defstruct [:from, :to, :isPrivate, :value]
 
-  def build(%Account{} = from_account, %Account{} = to_account) do
+  def build(%Account{} = from_account, %Account{} = to_account, value, is_private) do
     %__MODULE__{
       from: from_account,
-      to: to_account
+      to: to_account,
+      isPrivate: is_private,
+      value: value
     }
   end
 end
