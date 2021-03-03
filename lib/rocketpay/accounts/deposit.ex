fde86b3e958615e2eb6493fa196f44d4426576a3
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2021-03-03 12:37:37 -0300

    fix: remove double parenteses

--- lib/rocketpay/accounts/deposit.ex
+++ lib/rocketpay/accounts/deposit.ex
@@ -1,17 +1,46 @@
-defmodule Rocketpay.Accounts.Deposit do
-  alias Rocketpay.Accounts.Operation
-  alias Rocketpay.Repo
+defmodule Rockepay.Users.Create do
+  alias Ecto.Multi
+  alias Rocketpay.{Account, Repo, User}
 
   def call(params) do
-    params
-    |> Operation.call(:deposit)
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      account_changeset(repo, user)
+    end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
+      preload_data(repo, user)
+    end)
     |> run_transaction()
   end
 
+  defp insert_account(repo, user) do
+    user.id
+    |> User.changeset()
+    |> repo.insert()
+  end
+
+  defp account_changeset(_repo, %User{id: user_id}) do
+    params = %{user_id: user_id, balance: "0.00"}
+
+    Account.changeset(params)
+  end
+
+  defp preload_data(repo, user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
   defp run_transaction(multi) do
-    case Repo.transaction((multi)) do
+    case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{deposit: account}} -> {:ok, account}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{update_balance: account}} -> {:ok, account}
     end
   end
 end
